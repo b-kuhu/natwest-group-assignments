cbc3274b5c88c5b658a759045811c31d295e605b
Kuhu Bhatnagar <kuhubhatnagar19@gmail.com>
2024-02-11 12:51:29 +0530

    complete weather app

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,126 @@
+import "./App.css";
+import searchIcon from "./assets/search-icon.png";
+import cloudIcon from "./assets/cloudy.png";
+import windIcon from "./assets/wind.png";
+import humidyIcon from "./assets/humidity.png";
+import API_KEY1 from '.env';
+import React, { useState } from "react";
+
+import BarChart from "./BarChart";
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [searchHistory, setSearchHistory] = useState([]);
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const fetchData = async () => {
+    setError(null);
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY1}`;
+      const response = await fetch(url);
+
+      // Check if the response is successful
+      if (!response.ok) {
+        throw new Error("This city does not exist");
+      }
+
+      // Parse the JSON response
+      const jsonData = await response.json();
+      setData(jsonData);
+      //update search history
+      setSearchHistory((prevSearchHistory) => [
+        inputValue,
+        ...prevSearchHistory.slice(0, 4),
+      ]);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Fetch data only if inputValue is not empty
+    if (inputValue.trim() !== "") {
+      fetchData();
+    }
+  };
+
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
+
+  return (
+    <>
+    <h1>Weather App</h1>
+    <div className="container">
+      <div className="search-city">
+        <div className="input-container">
+          <input
+            type="text"
+            value={inputValue}
+            placeholder="Enter city name"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+          />
+          <button onClick={handleSearch}>
+            <img src={searchIcon} alt="search-icon" />
+          </button>
+        </div>
+        {isInputFocused && (
+          <div>
+            <ul className="search-history">
+              {searchHistory.map((search, index) => (
+                <>
+                  <li className="searched-record" key={index}>
+                    {search}
+                  </li>
+                  <hr />
+                </>
+              ))}
+            </ul>
+          </div>
+        )}
+        {error && <div>Error: {error.message}</div>}
+        {data && (
+          <div className="weather-details">
+            <img src={cloudIcon} alt="cloudy-icon" className="cloudy-icon" />
+            <div className="city-temperature">
+              {Math.round(data.main.temp) - 273} &#8451; /{" "}
+              {((Math.round(data.main.temp) - 273) * 9) / 5} &#x2109;
+            </div>
+            <div className="country-name">
+              <p>
+                {data.name},{data.sys.country}
+              </p>
+            </div>
+            <hr />
+
+            <div className="weather-display wind-speed">
+              <img src={windIcon} alt="wind-icon" />
+              <p>Wind Speed : {data.wind.speed}kmph</p>
+              <p></p>
+            </div>
+            <hr />
+            <div className="weather-display humidity">
+              <img src={humidyIcon} alt="humidity-icon" />
+              <p>Humidity : {data.main.humidity}%</p>
+            </div>
+          </div>
+        )}
+      </div>
+        {data && <BarChart key ={Math.random()+1} cityName={data.name} />}
+    </div>
+    </>
+  );
+};
+
+export default App;
---
+++ weather-app/src/BarChart.jsx
@@ -0,0 +1,227 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import API_KEY2 from '.env';
+  import { useEffect,useState } from 'react';
+
+import {Bar} from 'react-chartjs-2';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    maintainAspectRatio : false,
+    plugins: {
+      legend: {
+        position: 'right',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart',
+      },
+    },
+  };
+
+ 
+const BarChart = ({cityName}) =>{
+    const [data,setData] = useState({
+        labels: ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        datasets: [{
+          label: 'temperature',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            
+          ], borderColor: [
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)',
+              'rgb(255, 99, 132)'
+            ],
+            borderWidth: 1
+          },
+          {
+              label: 'humidity',
+              data: [60, 49, 88, 38, 76, 45, 50],
+              backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+
+              ], borderColor: [
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  'rgb(75, 192, 192)',
+                  
+                ],
+                borderWidth: 1
+              },
+              {
+                label: 'windspeed',
+                data: [60, 49, 88, 38, 76, 45, 50],
+                backgroundColor: [
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  
+                ], borderColor: [
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+                    'rgb(153, 102, 255)',
+            
+                  ],
+                  borderWidth: 1
+                }
+      ]
+        }
+
+    )
+    useEffect(()=>{
+        const fetchData = async() =>{
+            const url =`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}/2024-02-04/2024-02-11?unitGroup=us&include=days&key=${API_KEY2}&contentType=json`
+            const labelSet = [];
+            const dataSet1 = [];
+            const dataSet2 = [];
+            const dataSet3 = [];
+            await fetch(url)
+            .then(data =>{
+                const res = data.json();
+                return res;
+            }).then(res =>{
+                
+                for (let val= 0;val<res.days.length;val++) {
+                    dataSet1.push(((res.days[val].feelslike)*5)/9);
+                    dataSet2.push(res.days[val].humidity)
+                    dataSet3.push(res.days[val].windspeed)
+                    labelSet.push(res.days[val].datetime)
+                }
+                console.log(dataSet2);
+                setData({
+                    labels: labelSet,
+                    datasets: [{
+                      label: 'temperature',
+                      data: dataSet1,
+                      backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 205, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(201, 203, 207, 0.2)'
+                      ], borderColor: [
+                          'rgb(255, 99, 132)',
+                          'rgb(255, 159, 64)',
+                          'rgb(255, 205, 86)',
+                          'rgb(75, 192, 192)',
+                          'rgb(54, 162, 235)',
+                          'rgb(153, 102, 255)',
+                          'rgb(201, 203, 207)'
+                        ],
+                        borderWidth: 1
+                      },
+                      {
+                          label: 'humidity',
+                          data: dataSet2,
+                          backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 205, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(201, 203, 207, 0.2)'
+                          ], borderColor: [
+                              'rgb(255, 99, 132)',
+                              'rgb(255, 159, 64)',
+                              'rgb(255, 205, 86)',
+                              'rgb(75, 192, 192)',
+                              'rgb(54, 162, 235)',
+                              'rgb(153, 102, 255)',
+                              'rgb(201, 203, 207)'
+                            ],
+                            borderWidth: 1
+                          },
+                          {
+                            label: 'windspeed',
+                            data: dataSet3,
+                            backgroundColor: [
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              'rgba(54, 162, 235, 0.2)',
+                              
+                            ], borderColor: [
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                                'rgb(54, 162, 235)',
+                        
+                              ],
+                              borderWidth: 1
+                            }
+                  ]
+                })
+            })
+            .catch(e => {
+                console.log(e);
+            })    
+        }
+        fetchData();
+    },[cityName])
+    return <div className ='bar-graph' ><Bar data={data} options={options}/></div>
+}
+export default BarChart;
